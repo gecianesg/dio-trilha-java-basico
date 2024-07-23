c18a2a4b50d37728d69b0c999cf1673730ec9df5
Geciane Gonçalves <sgeciane27@gmail.com>
2024-07-22 23:45:08 -0300

    Correção erro

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -11,21 +11,21 @@ public class ContaTerminal {
 
         //Exibir a mensagem conta criada
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Digite o número da sua conta");
+            int numero = scanner.nextInt();
 
-        System.out.println("Digite o número da sua conta");
-        int numero = scanner.nextInt();
+            System.out.println("Digite o número da agência com o dígito");
+            String agencia = scanner.next();
 
-        System.out.println("Digite o número da agência com o dígito");
-        String agencia = scanner.next();
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
 
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
+            System.out.println("Digite o saldo");
+            Double saldo = scanner.nextDouble();
 
-        System.out.println("Digite o saldo");
-        Double saldo = scanner.nextDouble();
-
-        System.out.println("Olá " + nome +  " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "  +  numero +  " e seu saldo " + saldo +  " já está disponível para saque.");
+            System.out.println("Olá " + nome +  " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "  +  numero +  " e seu saldo " + saldo +  " já está disponível para saque.");
+        }
 
 
 
